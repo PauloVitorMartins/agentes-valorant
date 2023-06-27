49414904cdcf0379bd7ebdfecda4ddcaa5fbf5e0
PauloVitorMartins <pm885144@gmail.com>
2023-06-27 11:55:01 -0300

    fix-position-footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import '../css/footer.css'
+
+export default function Footer () {
+  return (
+    <>
+    <div className="footer-content">
+    <p>Made by Paulim</p>
+    <span>Baixe o jogo</span>
+    </div>
+    </>
+  )
+}
